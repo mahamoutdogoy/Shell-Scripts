4ae5255fe8ca6c4f82c5d306d3d6027649064d2b
Vlado Velev <vvelev86@gmail.com>
2017-04-05 20:39:17 +0300

    Oneliner Version

---
+++ Bash Scripts/Arrange Them All/arrange_them_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+a=(`find . -xdev -maxdepth 1 -type f`) 
+
+for i in ${a[*]} ; do
+	mkdir `basename $i`_dir 
+	mv $i `echo $i`_dir
+done
+
+rename 's/\_dir//g' *
+rename 's/\.[^\.]*$//' *
+rename 's/_/ /g' *
+
