05a05d1c72c8d31e44627008b178ed2d67753215
Vlado Velev <vvelev86@gmail.com>
2017-04-30 17:48:41 +0300

    Find HBA's WWN in AIX

--- Bash Scripts/Find HBA's WWN in AIX/Find_WWN_in_AIX.sh
+++ Bash Scripts/Find HBA's WWN in AIX/Find_WWN_in_AIX.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/biin/sh
 
 CSV="," ; for FCSX in `lscfg | grep fcs | awk '{ print $2 }'`;do echo ${FCSX}${CSV}`lscfg -vl ${FCSX} | grep "Network Address" | sed -e "s/^.*\.//"`${CSV}`lscfg -l ${FCSX} | awk '{ print $2 }'`; done
 
