0060b6c5710641034c05cc5324784e5b8533ed26
Vladislav Velev <vvelev86@gmail.com>
2017-04-30 18:18:24 +0300

    Update
    Update

--- Bash Scripts/Check RAID For Stale Extents/Check_RAID_For_Stale_Extents.sh
+++ Bash Scripts/Check RAID For Stale Extents/Check_RAID_For_Stale_Extents.sh
@@ -4,5 +4,5 @@ for vg in `vgdisplay | grep "VG Name" |awk '{print $3}'`; do \
 	for lv in `vgdisplay -v $vg |grep "LV Name"|awk '{print $3}'`; do \
 		echo "$lv has `lvdisplay -v $lv|grep stale|wc -l` stale extents"; \
 	done ; \
-done
+done 
 
