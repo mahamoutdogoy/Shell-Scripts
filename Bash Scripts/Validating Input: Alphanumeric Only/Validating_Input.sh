6809c8de4067f14a828efc8c56d6b25ea8fef5ea
Vlado Velev <vvelev86@gmail.com>
2017-05-27 20:41:21 +0300

    Validating Input

--- Bash Scripts/Validating Input: Alphanumeric Only/Validating_Input.sh
+++ Bash Scripts/Validating Input: Alphanumeric Only/Validating_Input.sh
@@ -7,7 +7,7 @@ read input
 validated_input=`echo "$input" | sed -e 's/[^[:alnum:]]//g'`
 
 if [[ "$input" != "$validated_input" ]] ; then 
-	echo "Please enter only letters and numbers." >&2
+	echo "Please enter only letters and numbers." 
 	exit 1
 else
 	echo "Input is valid."
