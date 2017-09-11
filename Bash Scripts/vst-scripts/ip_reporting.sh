c71b9725c559f0d57170797a65c1b46c9471e1b0
Vladislav Velev <vvelev86@gmail.com>
2017-09-11 20:27:53 +0300

    Update ip_reporting.sh

--- Bash Scripts/vst-scripts/ip_reporting.sh
+++ Bash Scripts/vst-scripts/ip_reporting.sh
@@ -5,3 +5,4 @@ exit 123
 
 node_ip=`ifconfig | head -2 | tail -1 | awk {'print $2'} | sed -e 's/addr\://g'`
 ssh 192.168.0.106 "echo $node_ip >> /tmp/ip_nodes.txt" && touch /root/ip_reporting/ip_reporting.lock
+
